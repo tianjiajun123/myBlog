616b135d8ecf526123c73525f9089c39ee1bb20b
Your Name <you@example.com>
2021-01-03 20:16:57 +0800

    第二次提交

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Articles
 # Register your models here.
+
+class ArticlesAdmin(admin.ModelAdmin):
+    list_display=("title","author","img","abstract","visited","created_at")
+    search_fields=("title","author","abstract","visited","created_at")
+    list_filter=list_display
+
+
+
+admin.site.register(Articles,ArticlesAdmin)

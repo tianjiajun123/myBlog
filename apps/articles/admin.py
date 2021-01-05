98101f5c3db98917608cbe2819d7a63f2bfa5352
Your Name <you@example.com>
2021-01-05 20:24:15 +0800

    1月5推送

--- apps/articles/admin.py
+++ apps/articles/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Articles
+from .models import Articles,Category,Tag
 # Register your models here.
 
 class ArticlesAdmin(admin.ModelAdmin):
@@ -10,3 +10,5 @@ class ArticlesAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Articles,ArticlesAdmin)
+admin.site.register(Category,)
+admin.site.register(Tag,)
